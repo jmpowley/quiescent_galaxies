cacee0a8ce49b7cca3b5f75d14c5002ac3eae25f
jmpowley <jonahpowley1@gmail.com>
2025-11-07 14:33:20 +0000

    Changed fit types to "independent" and "simultaneous"

--- scalpel/src/scalpel/scalpel.py
+++ scalpel/src/scalpel/scalpel.py
@@ -8,25 +8,34 @@ class Scalpel:
 
         self.config = config
 
+    def fit_bands(self):
+        """Fit photometric bands to obtain structural parameters"""
+
+        # Load config info
+        cutout_kwargs = self.config["cutout_kwargs"]
+        prior_dict = self.config["prior_dict"]
+        fit_kwargs = self.config["fit_kwargs"]
+        fit_type = fit_kwargs["fit_type"]
+        
+        # Fit photometric bands depending on type
+        # -- independently
+        if fit_type == "independent":
+            fit_independent_bands(cutout_kwargs=cutout_kwargs, prior_dict=prior_dict, **fit_kwargs)
+        # -- simultaneously
+        elif fit_type == "simultaneous":
+            fit_simultaneous_bands(cutout_kwargs=cutout_kwargs, prior_dict=prior_dict, **fit_kwargs)
+        else:
+            raise ValueError("Options for fitting are 'independent' or 'simultaneous'")
+
     def dissect(self):
+        """Wrapper for scalpel. Prepares data for fitting and then runs dissection procedure"""
 
         # Load config info
         cutout_kwargs = self.config["cutout_kwargs"]
         prior_dict = self.config["prior_dict"]
         fit_kwargs = self.config["fit_kwargs"]
 
-        fit_type = fit_kwargs["fit_type"]
+        # Step 1: fit photometric bands to obtain structural parameters
+        self.fit_bands()
 
-        # Fit photometric bands depending on type
-        # -- individual
-        if fit_type == "individual":
-            fit_independent_bands(cutout_kwargs=cutout_kwargs, prior_dict=prior_dict, **fit_kwargs)
-        # -- multiple
-        if fit_type == "multiple":
-            fit_simultaneous_bands(cutout_kwargs=cutout_kwargs, prior_dict=prior_dict, **fit_kwargs)
-
-    def run_scalpel(self):
-        """Wrapper for scalpel. Prepares data for fitting and then runs dissection procedure"""
-
-        # Load data
-        self.dissect()
+        # Step 2: extract structural components from results
