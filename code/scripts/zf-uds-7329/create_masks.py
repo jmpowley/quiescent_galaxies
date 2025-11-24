375e20149546df3cbb421c131b33790126c74f40
jmpowley <jonahpowley1@gmail.com>
2025-11-24 10:08:39 +0000

    Added full plotting function

--- code/scripts/zf-uds-7329/create_masks.py
+++ code/scripts/zf-uds-7329/create_masks.py
@@ -81,7 +81,6 @@ def create_1d_wave_mask(wave, wave_units : str, mask_dict : dict, mask_units, de
     # Apply mask dictionary to mask array
     for key, val in mask_dict.items():
         key_mask = np.zeros(wave.shape, dtype=bool)
-
         # -- range entries
         if key == "range":
             for entry in val:
@@ -97,8 +96,6 @@ def create_1d_wave_mask(wave, wave_units : str, mask_dict : dict, mask_units, de
 
         # Combine key_mask with overall mask
         mask[key_mask] = not default
-
-    print(mask)
 
     if return_ints:
         return mask.astype(int)
@@ -122,7 +119,7 @@ def save_wave_mask(mask, out_dir : str,out_file : str, mask_ext : str):
     hdul.writeto(out_path, overwrite=True)
     print(f"Mask saved to {out_path}")
 
-def plot_masked_regions(ax, wave, mask, color='gray', alpha=0.3, default=True):
+def plot_mask(wave, flux, mask, zred=None, color='gray', alpha=0.3, default=True):
     """
     Plots shaded regions (axvspan) for contiguous wavelength regions 
     where the mask is default.
@@ -130,186 +127,170 @@ def plot_masked_regions(ax, wave, mask, color='gray', alpha=0.3, default=True):
     wave = np.asarray(wave)
     mask = np.asarray(mask)
 
+    # Create figure
+    fig, ax = plt.subplots(1, 1, figsize=(8, 6))
+
     # Optionally invert mask to highlight where mask is not default
-    # for Prospector, masked regions are False
-    if default is not False:
+    if default is True:
         mask = ~mask
 
-    # Identify transitions between masked/unmasked regions
+    # Process mask
+    # -- identify mask/unmasked transitions
     diff = np.diff(mask.astype(int))
-    start_indices = np.where(diff == 1)[0] + 1
-    end_indices   = np.where(diff == -1)[0] + 1
-
-    # Handle cases where mask starts/ends inside a masked region
+    start_indices = np.where(diff==1)[0] + 1
+    end_indices = np.where(diff==-1)[0] + 1
+    # -- handle cases where mask starts/ends inside a masked region
     if mask[0]:
         start_indices = np.insert(start_indices, 0, 0)
     if mask[-1]:
         end_indices = np.append(end_indices, len(mask) - 1)
 
-    # Plot spans
+    # Plot spectra
+    ax.step(wave, flux, color="black", where="mid")
+
+    # Plot mask
     for start, end in zip(start_indices, end_indices):
         ax.axvspan(wave[start], wave[end], color=color, alpha=alpha)
 
-def return_file_from_param_dict(param_dict, obs_type):
+    # Prettify
+    ax.set_ylim(0, None)
+    ax.set_xticks(np.arange(1, 6))
+    if zred is not None:
+        ax_top = ax.twiny()
+        ax_top.set_xlim(ax.get_xlim())
+        top_ticks_rest = np.arange(0.2, 1.4, 0.2)
+        top_ticks_obs = top_ticks_rest * (1 + zred)
+        ax_top.set_xticks(top_ticks_obs)
+        ax_top.set_xticklabels([f"{t:.1f}" for t in top_ticks_rest])
+    ax.set_xlabel(r'Observed Wavelength [$\mu$m]', size=18)
+    ax.set_ylabel(r'$f_\lambda~[~10^{-19}$ erg s$^{-1}$ cm$^{-2}$ Å$^{-1}]$', size=18)
 
-    # Prism
-    if obs_type == 'prism':
-        # -- extract variables
-        name = param_dict["name"]
-        version = param_dict["version"]
-        nod = param_dict["nod"]
-        # -- version of spectra/reduction
-        if version is not None:
-            version_str = f"_{version}"
-        else:
-            version_str = ""
-        # -- nod
-        if param_dict["nod"] is not None:
-            nod_str = f"_{nod}"
-        else:
-            nod_str = ""
-        # -- spectrum dimensions
-        dim_str = "_1D"
-        # -- combine into file
-        extra_str = version_str + nod_str + dim_str
-        out_file = f"{name}_prism_clear{version_str}{nod_str}{dim_str}.fits"
-    # Grating
-    elif obs_type == 'grating':
-        # -- extract variables
-        name = filter = param_dict["name"]
-        grating = param_dict["grating"]
-        filter = param_dict["filter"]
-        version = param_dict["version"]
-        nod = param_dict["nod"]
-        # -- version of spectra/reduction
-        if version is not None:
-            version_str = f"_{version}"
-        else:
-            version_str = ""
-        # -- nod
-        if param_dict["nod"] is not None:
-            nod_str = f"_{nod}"
-        else:
-            nod_str = ""
-        # -- spectrum dimensions
-        dim_str = "_1D"
-        # -- combine into file
-        extra_str = version_str + nod_str + dim_str
-        out_file = f"{name}_{grating}_{filter}{version_str}{nod_str}{dim_str}.fits"
-    else:
-        raise ValueError("Observation type must be 'prism' or 'grating'")
-
-    return out_file
+    return fig
 
 # Load spectra
 obs_kwargs = {
 
-    "prism_params" : {
-        "prism_dir" : "/Users/Jonah/PhD/Research/quiescent_galaxies/data_processed/zf-uds-7329/spectra",
-        "name" : "007329",
-        "version" : "v3.1",
-        "nod" : "extr5",
+    "phot_kwargs" : {
+        "data_dir" : "/Users/Jonah/PhD/Research/quiescent_galaxies/data_processed/zf-uds-7329/photometry",
+        "data_name" : "007329_nircam_photometry.fits",
         "data_ext" : "DATA",
+        "in_flux_units" : "magnitude",
+        "out_flux_units" : "cgs",
+        "snr_limit" : 20.0,
+        "prefix" : "phot",
+    },
+
+    "prism_kwargs" : {
+        "data_dir" : "/Users/Jonah/PhD/Research/quiescent_galaxies/data_processed/zf-uds-7329/spectra",
+        "data_name" : "007329_prism_clear_v3.1_extr5_1D.fits",
+        "data_ext" : "DATA",
+        "mask_dir" : None,
+        "mask_name" : None,
         "mask_ext" : None,
+        "disp_dir" : "/Users/Jonah/PhD/Research/quiescent_galaxies/data_processed/dispersion_curves",
+        "disp_name" : "uds7329_nirspec_prism_disp.fits",
         "in_wave_units" : "si",
         "out_wave_units" : "um",
         "in_flux_units" : "si",
         "out_flux_units" : "cgs",
         "rescale_factor" : 1.86422,
-        "snr_limit" : 20,
-        "return_none" : False,
+        "snr_limit" : 20.0,
+        "prefix" : "prism",
     },
 
-    "grat1_params" : {
-        "grating_dir" : "/Users/Jonah/PhD/Research/quiescent_galaxies/data_processed/zf-uds-7329/spectra",
-        "name" : "007329",
-        "grating" : "g140m",
-        "filter" : "f100lp",
-        "version" : None,
-        "nod" : None,
+    "grat1_kwargs" : {
+        "data_dir" : "/Users/Jonah/PhD/Research/quiescent_galaxies/data_processed/zf-uds-7329/spectra",
+        "data_name" : "007329_g140m_f100lp_1D.fits",
         "data_ext" : "DATA",
-        "mask_ext" : "VALID",
+        "mask_dir" : None,
+        "mask_name" : None,
+        "mask_ext" : None,
+        "disp_dir" : "/Users/Jonah/PhD/Research/quiescent_galaxies/data_processed/dispersion_curves",
+        "disp_name" : "jwst_nirspec_g140m_disp.fits",
+        "in_wave_units" : "um",
+        "out_wave_units" : "um",
+        "in_flux_units" : "ujy",
+        "out_flux_units" : "cgs",
+        "rescale_factor" : 1.86422,
+        "snr_limit" : 20.0,
+        "prefix" : "g140m",
+    },
+
+    "grat2_kwargs" : {
+        "data_dir" : "/Users/Jonah/PhD/Research/quiescent_galaxies/data_processed/zf-uds-7329/spectra",
+        "data_name" : "007329_g235m_f170lp_1D.fits",
+        "data_ext" : "DATA",
+        "mask_dir" : None,
+        "mask_name" : None,
+        "mask_ext" : None,
+        "disp_dir" : "/Users/Jonah/PhD/Research/quiescent_galaxies/data_processed/dispersion_curves",
+        "disp_name" : "jwst_nirspec_g235m_disp.fits",
         "in_wave_units" : "um",
         "out_wave_units" : "um",
         "in_flux_units" : "ujy",
         "out_flux_units" : "cgs",
         "rescale_factor" : 1.86422,
         "snr_limit" : 20,
-        "return_none" : False,
+        "prefix" : "g235m",
     },
 
-    "grat2_params" : {
-        "grating_dir" : "/Users/Jonah/PhD/Research/quiescent_galaxies/data_processed/zf-uds-7329/spectra",
-        "name" : "007329",
-        "grating" : "g235m",
-        "filter" : "f170lp",
-        "version" : None,
-        "nod" : None,
+    "grat3_kwargs" : {
+        "data_dir" : "/Users/Jonah/PhD/Research/quiescent_galaxies/data_processed/zf-uds-7329/spectra",
+        "data_name" : "007329_g395m_f290lp_1D.fits",
         "data_ext" : "DATA",
-        "mask_ext" : "VALID",
+        "mask_dir" : None,
+        "mask_name" : None,
+        "mask_ext" : None,
+        "disp_dir" : "/Users/Jonah/PhD/Research/quiescent_galaxies/data_processed/dispersion_curves",
+        "disp_name" : "jwst_nirspec_g140m_disp.fits",
         "in_wave_units" : "um",
         "out_wave_units" : "um",
         "in_flux_units" : "ujy",
         "out_flux_units" : "cgs",
         "rescale_factor" : 1.86422,
-        "snr_limit" : 20,
-        "return_none" : False,
-    },
-
-    "grat3_params" : {
-        "grating_dir" : "/Users/Jonah/PhD/Research/quiescent_galaxies/data_processed/zf-uds-7329/spectra",
-        "name" : "007329",
-        "grating" : "g395m",
-        "filter" : "f290lp",
-        "version" : None,
-        "nod" : None,
-        "data_ext" : "DATA",
-        "mask_ext" : "VALID",
-        "in_wave_units" : "um",
-        "out_wave_units" : "um",
-        "in_flux_units" : "ujy",
-        "out_flux_units" : "cgs",
-        "rescale_factor" : 1.86422,
-        "snr_limit" : 20,
-        "return_none" : False,
+        "snr_limit" : 20.0,
+        "prefix" : "g395m",
+        "add_jitter" : True,
+        "include_outliers" : True,
+        "fit_obs" : False,
     },
 }
 
 # Redshift information
-zred = 3.2
+zred = 3.19
 
 # Load data
-prism_params = obs_kwargs['prism_params']
-grat1_params = obs_kwargs['grat1_params']
-grat2_params = obs_kwargs['grat2_params']
-grat3_params = obs_kwargs['grat3_params']
+prism_kwargs = obs_kwargs['prism_kwargs']
+grat1_kwargs = obs_kwargs['grat1_kwargs']
+grat2_kwargs = obs_kwargs['grat2_kwargs']
+grat3_kwargs = obs_kwargs['grat3_kwargs']
 
-prism_wave, prism_flux, prism_err,_ = load_prism_data(**prism_params)
-grat1_wave, grat1_flux, grat1_err,_ = load_grating_data(**grat1_params)
-grat2_wave, grat2_flux, grat2_err,_ = load_grating_data(**grat2_params)
-grat3_wave, grat3_flux, grat3_err,_ = load_grating_data(**grat3_params)
+prism_wave_um, prism_flux, prism_err = load_prism_data(**prism_kwargs)
+grat1_wave_um, grat1_flux, grat1_err = load_grating_data(**grat1_kwargs)
+grat2_wave_um, grat2_flux, grat2_err = load_grating_data(**grat2_kwargs)
+grat3_wave_um, grat3_flux, grat3_err = load_grating_data(**grat3_kwargs)
 
 # Create prism mask
 prism_mask_dict_A = {
     'range' : [
-        [convert_wave_um_to_A(prism_wave.min()), convert_wave_um_to_A(1)],
-        [convert_wave_um_to_A(5), convert_wave_um_to_A(prism_wave.max())],
+        [convert_wave_um_to_A(prism_wave_um.min()), convert_wave_um_to_A(1)],
+        [convert_wave_um_to_A(5), convert_wave_um_to_A(prism_wave_um.max())],
         ],
     'line' : [
-        [5900, 150]  # sodium doublet
+        [5900, 400]  # sodium doublet
         ],
 }
-prism_mask = create_1d_wave_mask(wave=prism_wave, wave_units="um", mask_dict=prism_mask_dict_A, mask_units="A",
+prism_mask = create_1d_wave_mask(wave=prism_wave_um, wave_units="um", mask_dict=prism_mask_dict_A, mask_units="A",
                               default=True, redshift_lines=True, z=zred, return_ints=True)
 
 print("grat1")
 # Create grat1 mask
 grat1_mask_dict_A = {
     'range' : [
-        [convert_wave_um_to_A(grat1_wave.min()), convert_wave_um_to_A(1)],
+        [convert_wave_um_to_A(grat1_wave_um.min()), convert_wave_um_to_A(1)],
         ]
 }
-grat1_mask = create_1d_wave_mask(wave=grat1_wave, wave_units="um", mask_dict=grat1_mask_dict_A, mask_units="A",
+grat1_mask = create_1d_wave_mask(wave=grat1_wave_um, wave_units="um", mask_dict=grat1_mask_dict_A, mask_units="A",
                               default=True, redshift_lines=True, z=zred, return_ints=True)
 
 # Create grat2 mask
@@ -321,95 +302,34 @@ print("grat3")
 # Create grat3 mask
 grat3_mask_dict_A = {
     'range' : [
-        [convert_wave_um_to_A(grat3_wave.min()), convert_wave_um_to_A(2.75)],
-        [convert_wave_um_to_A(5.4), convert_wave_um_to_A(grat3_wave.max())],
-        [convert_wave_um_to_A(prism_wave.max()), convert_wave_um_to_A(grat3_wave.max())],  # cut off at prism limit
+        [convert_wave_um_to_A(grat3_wave_um.min()), convert_wave_um_to_A(2.75)],
+        [convert_wave_um_to_A(5.4), convert_wave_um_to_A(grat3_wave_um.max())],
+        [convert_wave_um_to_A(prism_wave_um.max()), convert_wave_um_to_A(grat3_wave_um.max())],  # cut off at prism limit
         ]
 }
-grat3_mask = create_1d_wave_mask(wave=grat3_wave, wave_units="um", mask_dict=grat3_mask_dict_A, mask_units="A",
+grat3_mask = create_1d_wave_mask(wave=grat3_wave_um, wave_units="um", mask_dict=grat3_mask_dict_A, mask_units="A",
                               default=True, redshift_lines=True, z=zred, return_ints=True)
 
-# Prism
-fig, ax = plt.subplots(1, 1, figsize=(8, 6))
-# -- plot spectra and mask
-ax.step(prism_wave, prism_flux, color="black", where="mid")
-plot_masked_regions(ax, prism_wave, prism_mask, default=True)
-# -- prettify
-ax.set_ylim(0, None)
-ax.set_xticks(np.arange(1, 6))
-if zred is not None:
-    ax_top = ax.twiny()
-    ax_top.set_xlim(ax.get_xlim())
-    top_ticks_rest = np.arange(0.2, 1.4, 0.2)
-    top_ticks_obs = top_ticks_rest * (1 + zred)
-    ax_top.set_xticks(top_ticks_obs)
-    ax_top.set_xticklabels([f"{t:.1f}" for t in top_ticks_rest])
-ax.set_xlabel(r'Observed Wavelength [$\mu$m]', size=18)
-ax.set_ylabel(r'$f_\lambda~[~10^{-19}$ erg s$^{-1}$ cm$^{-2}$ Å$^{-1}]$', size=18)
-
-# Grat1
-fig, ax = plt.subplots(1, 1, figsize=(8, 6))
-# -- plot spectra and mask
-ax.step(grat1_wave, grat1_flux, color="black", where="mid")
-plot_masked_regions(ax, grat1_wave, grat1_mask, default=True)
-# -- prettify
-ax.set_ylim(0, None)
-ax.set_xticks(np.arange(1, 6))
-if zred is not None:
-    ax_top = ax.twiny()
-    ax_top.set_xlim(ax.get_xlim())
-    top_ticks_rest = np.arange(0.2, 1.4, 0.2)
-    top_ticks_obs = top_ticks_rest * (1 + zred)
-    ax_top.set_xticks(top_ticks_obs)
-    ax_top.set_xticklabels([f"{t:.1f}" for t in top_ticks_rest])
-ax.set_xlabel(r'Observed Wavelength [$\mu$m]', size=18)
-ax.set_ylabel(r'$f_\lambda~[~10^{-19}$ erg s$^{-1}$ cm$^{-2}$ Å$^{-1}]$', size=18)
-
-# Grat2
-fig, ax = plt.subplots(1, 1, figsize=(8, 6))
-# -- plot spectra and mask
-ax.step(grat2_wave, grat2_flux, color="black", where="mid")
-# plot_masked_regions(ax, grat2_wave, grat2_mask, mask_default=True)
-# -- prettify
-ax.set_ylim(0, None)
-ax.set_xticks(np.arange(1, 6))
-if zred is not None:
-    ax_top = ax.twiny()
-    ax_top.set_xlim(ax.get_xlim())
-    top_ticks_rest = np.arange(0.2, 1.4, 0.2)
-    top_ticks_obs = top_ticks_rest * (1 + zred)
-    ax_top.set_xticks(top_ticks_obs)
-    ax_top.set_xticklabels([f"{t:.1f}" for t in top_ticks_rest])
-ax.set_xlabel(r'Observed Wavelength [$\mu$m]', size=18)
-ax.set_ylabel(r'$f_\lambda~[~10^{-19}$ erg s$^{-1}$ cm$^{-2}$ Å$^{-1}]$', size=18)
-
-# Grat3
-fig, ax = plt.subplots(1, 1, figsize=(8, 6))
-# -- plot spectra and mask
-ax.step(grat3_wave, grat3_flux, color="black", where="mid")
-plot_masked_regions(ax, grat3_wave, grat3_mask, default=True)
-# -- prettify
-ax.set_ylim(0, None)
-ax.set_xticks(np.arange(1, 6))
-if zred is not None:
-    ax_top = ax.twiny()
-    ax_top.set_xlim(ax.get_xlim())
-    top_ticks_rest = np.arange(0.2, 1.4, 0.2)
-    top_ticks_obs = top_ticks_rest * (1 + zred)
-    ax_top.set_xticks(top_ticks_obs)
-    ax_top.set_xticklabels([f"{t:.1f}" for t in top_ticks_rest])
-ax.set_xlabel(r'Observed Wavelength [$\mu$m]', size=18)
-ax.set_ylabel(r'$f_\lambda~[~10^{-19}$ erg s$^{-1}$ cm$^{-2}$ Å$^{-1}]$', size=18)
+# Plot masks
+# -- prism
+fig = plot_mask(prism_wave_um, prism_flux, prism_mask, zred=zred)
+# -- grat1
+fig = plot_mask(grat1_wave_um, grat1_flux, grat1_mask, zred=zred)
+# -- grat2
+# fig = plot_masked_regions(grat2_wave_um, grat2_flux, grat2_mask)
+# -- grat3
+fig = plot_mask(grat3_wave_um, grat3_flux, grat3_mask, zred=zred)
 
 plt.show()
 
 # Save masks
 out_dir = "/Users/Jonah/PhD/Research/quiescent_galaxies/outputs/zf-uds-7329/wave_masks"
 # -- prism
-prism_file = return_file_from_param_dict(param_dict=prism_params, obs_type="prism")
-out_file = prism_file.strip(".fits") + "_mask" + ".fits"  # modify output file name
+out_file = prism_kwargs["data_name"].replace(".fits", "_mask.fits")
 save_wave_mask(prism_mask, out_dir, out_file, mask_ext="MASK")
 # -- grat1
-prism_file = return_file_from_param_dict(param_dict=grat1_params, obs_type="grat1")
-out_file = prism_file.strip(".fits") + "_mask" + ".fits"  # modify output file name
-save_wave_mask(prism_mask, out_dir, out_file, mask_ext="MASK")
+out_file = grat1_kwargs["data_name"].replace(".fits", "_mask.fits")
+save_wave_mask(grat1_mask, out_dir, out_file, mask_ext="MASK")
+# -- grat3
+out_file = grat3_kwargs["data_name"].replace(".fits", "_mask.fits")
+save_wave_mask(grat3_mask, out_dir, out_file, mask_ext="MASK")
